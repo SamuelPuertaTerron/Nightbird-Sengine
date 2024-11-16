c025627c43d395f85975a0bda700a41100960001
Sam Puerta Terron <spuertaterron@gmail.com>
2024-11-16 14:53:10 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ setlocal
 rem Define a label for error handling
 :Error
 
-call Third-Party\premake\premake5.exe vs2022
+call premake5.exe vs2022
 
 rem Check the error code of the last command
 if %errorlevel% neq 0 (
